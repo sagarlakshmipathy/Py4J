42897fef0d248f28e8c1359a0636cdfa1def08d3
Sagar Lakshmipathy <18vidhyasagar@gmail.com>
2023-12-04 14:33:34 -0800

    add long desc in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="py4j-example",
     version="0.1.0",
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
