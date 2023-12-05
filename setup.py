44018a99bde9abe93384c55e2a3c025fb4fd6409
Sagar Lakshmipathy <18vidhyasagar@gmail.com>
2023-12-04 17:54:46 -0800

    change package version in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
+import os
+import sys
 from setuptools import setup, find_packages
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+version_file = os.path.join(this_directory, 'py4j_example/version.py')
+
+try:
+    exec(open(version_file).read())
+except IOError:
+    print(f"Failed to load version file for packaging. {version_file} not found!", file=sys.stderr)
+    sys.exit(-1)
+VERSION = __version__
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="py4j-example",
-    version="0.1.0",
+    version=VERSION,
     packages=find_packages(),
     package_data={"py4j_example": ["jars/*.jar"]},  # Include JAR files
     long_description=long_description,
